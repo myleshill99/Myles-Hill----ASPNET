a39ed28d893da86bfff5e1af07ef0d0ed10e858e
Myles Hill <mylehill99@gmail.com>
2024-02-15 11:44:32 -0500

    finished

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -7,5 +7,7 @@ namespace Testing
     public interface IProductRepository
     {
         IEnumerable<Product> GetAllProducts();
+        Product GetProduct(int id);
+        void UpdateProduct(Product product);
     }
 }
